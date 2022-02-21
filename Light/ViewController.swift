5ac4a30d5818ac93aa07e82cbfc36a7f0403305c
Manzaraz <magnusmanz@gmail.com>
2022-02-21 19:39:05 -0300

    Swift: first contact

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var lightOn = true
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
-
+    
+    fileprivate func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
+    }
+    
+    
 
 }
-
